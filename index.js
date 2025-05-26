6259ed77f65abc6a8ef07dd28a88b5335b0e9f8f
Hayden Baca <therealprogrammer4444@gmail.com>
2025-05-26 13:51:13 -0500

    Implemented page switching by clicking the submit button

---
+++ index.js
@@ -0,0 +1,23 @@
+let rating;
+
+const card = document.querySelector('.card');
+
+document.querySelector('.js-submit-button')
+  .addEventListener('click', () => {
+    card.removeChild(card.querySelector('.rating-section'));
+
+    card.insertAdjacentHTML('beforeend', `
+    <section class="thank-you-section">
+      <img src="/images/illustration-thank-you.svg" alt="Thank You Image" class="thank-you-image">
+
+      <p class="rating-tag">You selected ${rating} out of 5</p>
+      
+      <h1 class="thank-you-heaer">Thank you!</h1>
+
+      <p class="thank-you-description">
+        We appreciate you taking the time to give a rating. If you ever need more support, 
+        don’t hesitate to get in touch!
+      </p>
+    </section>
+    `);
+  });
