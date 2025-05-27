05ff4dd12851ce00dd364cb1ace436ede470d2f9
Hayden Baca <therealprogrammer4444@gmail.com>
2025-05-26 19:44:50 -0500

    Fixed flicker via using display instead of HTML insertion on sections

--- index.js
+++ index.js
@@ -5,22 +5,10 @@ const card = document.querySelector('.card');
 /*handle submit button*/
 document.querySelector('.js-submit-button')
   .addEventListener('click', () => {
-    card.removeChild(card.querySelector('.rating-section'));
+    card.querySelector('.thank-you-section').classList.remove('hidden');
+    card.querySelector('.rating-section').classList.add('hidden');
 
-    card.insertAdjacentHTML('beforeend', `
-    <section class="thank-you-section">
-      <img src="/images/illustration-thank-you.svg" alt="Thank You Image" class="thank-you-image">
-
-      <p class="rating-tag">You selected ${rating} out of 5</p>
-      
-      <h1 class="thank-you-header">Thank you!</h1>
-
-      <p class="thank-you-description">
-        We appreciate you taking the time to give a rating. If you ever need more support, 
-        don’t hesitate to get in touch!
-      </p>
-    </section>
-    `);
+    card.querySelector('.rating-tag').innerText = `You selected ${rating} out of 5`;
 
     card.classList.toggle('thanks-section-padded');
   });
